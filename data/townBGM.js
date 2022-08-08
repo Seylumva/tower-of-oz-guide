08259eb0721b896ef30bceaedb3d9a269955c9c7
SeylumVA <sc2nem@gmail.com>
2022-08-07 23:04:30 -0400

    First build of the guide

---
+++ data/townBGM.js
@@ -0,0 +1,106 @@
+export default [
+  {
+    town: "Ariant",
+    videoId: "w1RgDSoOajw",
+  },
+  {
+    town: "Aquarium",
+    videoId: "qtw0sIBLjrw",
+  },
+  {
+    town: "Edelstein",
+    videoId: "Iayh6shDnwQ",
+  },
+  {
+    town: "El Nath",
+    videoId: "X6X7U5V9Obk",
+  },
+  {
+    town: "Ellinia",
+    videoId: "gfgBDs8z6WE",
+  },
+  {
+    town: "Elluel",
+    videoId: "5gxVQPSbkJw",
+  },
+  {
+    town: "Ereve",
+    videoId: "3r9s43TG9yA",
+  },
+  {
+    town: "Fox Point Village",
+    videoId: "lorm88ckSuM",
+  },
+  {
+    town: "Heliseum Reclamation HQ",
+    videoId: "CuoeZkWgDjk",
+  },
+  {
+    town: "Henesys",
+    videoId: "s2_MAplvHeQ",
+  },
+  {
+    town: "Herb Town",
+    videoId: "uv8QObpL2EY",
+  },
+  {
+    town: "Kerning City",
+    videoId: "5gxVQPSbkJw",
+  },
+  {
+    town: "Korean Folk Town",
+    videoId: "KK_IjvtszTA",
+  },
+  {
+    town: "Leafre",
+    videoId: "theIdIhZzVE",
+  },
+  {
+    town: "Lith Harbor",
+    videoId: "F6LIFBVhObQ",
+  },
+  {
+    town: "Ludibrium",
+    videoId: "49AZqVhXVeU",
+  },
+  {
+    town: "Lumiere",
+    videoId: "ZpVpjZJyDJA",
+  },
+  {
+    town: "Magatia",
+    videoId: "5FDFLwPw0bU",
+  },
+  {
+    town: "Mu Lung",
+    videoId: "2VMCpGe9MGc",
+  },
+  {
+    town: "Nautilus",
+    videoId: "iZaVKkfuD6s",
+  },
+  {
+    town: "Omega Sector",
+    videoId: "LxQt8EVhBOs",
+  },
+  {
+    town: "Orbis",
+    videoId: "DThWMbPZXy8",
+  },
+  {
+    town: "Pantheon",
+    videoId: "ZYeUnqsCnxg",
+  },
+  {
+    town: "Perion",
+    videoId: "9lwOsX763N0",
+  },
+  {
+    town: "Rien",
+    videoId: "YDfIfL_Fxec",
+  },
+  {
+    town: "Sleepywood",
+    videoId: "tWCWIhA3XQw",
+  },
+];
